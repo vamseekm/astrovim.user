c5cb622d4cfbe44afd9403a87f08be3ed1a728f1
Vamsee <vamsee@Vamsee.local>
2023-07-19 03:40:10 +0530

    customizations

---
+++ init.lua
@@ -0,0 +1,9 @@
+return {
+  plugins = { 
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+    { import = "astrocommunity.colorscheme.everforest" },
+    {import = "astrocommunity.lsp.lsp-inlayhints-nvim"},
+  },
+  colorscheme="everforest",
+}
