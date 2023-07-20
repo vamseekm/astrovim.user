1ce05c165f80410f53a7919108237739687eef90
Vamsee <vamsee@Vamsee.local>
2023-07-20 14:30:56 +0530

    add more plugs

--- init.lua
+++ init.lua
@@ -3,7 +3,13 @@ return {
     "AstroNvim/astrocommunity",
     { import = "astrocommunity.colorscheme.tokyonight-nvim" },
     { import = "astrocommunity.colorscheme.everforest" },
-    {import = "astrocommunity.lsp.lsp-inlayhints-nvim"},
+    { import = "astrocommunity.lsp.lsp-inlayhints-nvim" },
+    { import = "astrocommunity.pack.json" },
+    { import = "astrocommunity.pack.rust" },
+    { import = "astrocommunity.pack.tailwindcss" },
+    { import = "astrocommunity.pack.typescript" },
+    { import = "astrocommunity.pack.python" },
+    { import = "astrocommunity/utility/noice-nvim" }
   },
   colorscheme="everforest",
 }
